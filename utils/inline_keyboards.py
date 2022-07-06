d2814fd14190da1101ce6c8e8ffe466ce607d978
Ivan <belogurov.ivan@list.ru>
2022-07-06 19:13:47 +0500

    created scrolling pages of buttons in the shop and admin-panel

--- utils/inline_keyboards.py
+++ utils/inline_keyboards.py
@@ -20,20 +20,52 @@ shop_keyboard.add(InlineKeyboardButton(text="Выйти", callback_data="exit_fr
 
 
 async def get_all_goods_keyboard(mode):
-    all_goods_keyboard = InlineKeyboardMarkup()
+    all_goods_keyboards = {}
+    all_buttons = []
     goods = await get_all_goods()
+    callback = None
+    page = 1
+
+    if mode == "get":
+        callback = get_good_callback
+
+    elif mode == "remove":
+        callback = remove_good_callback
 
     for name, author, id in goods:
-        callback = None
+        all_buttons.append(InlineKeyboardButton(text=f"{name} | {author}", callback_data=callback.new(id)))
 
-        if mode == "get":
-            callback = get_good_callback.new(id)
+    while len(all_buttons) > 1:
+        keyboard = InlineKeyboardMarkup()
+        counter = 1
 
-        elif mode == "remove":
-            callback = remove_good_callback.new(id)
+        try:
+            while counter < 5:
+                keyboard.add(all_buttons[0])
+                all_buttons.remove(all_buttons[0])
+                counter += 1
 
-        all_goods_keyboard.add(InlineKeyboardButton(text=f"{name} | {author}", callback_data=callback))
+        except:
+            if page != 1:
+                keyboard.add(InlineKeyboardButton(text="Назад", callback_data="previous_page"))
 
-    all_goods_keyboard.add(InlineKeyboardButton(text="Назад", callback_data="back_to_shop_menu"))
+            else:
+                keyboard.add(InlineKeyboardButton(text="Назад", callback_data="back_to_shop_menu"))
 
-    return all_goods_keyboard
+            break
+
+        else:
+            if page == 1:
+                keyboard.add(InlineKeyboardButton(text="Назад", callback_data="back_to_shop_menu"))
+
+            else:
+                keyboard.add(InlineKeyboardButton(text="Назад", callback_data="previous_page"))
+
+            if len(all_buttons) > 0:
+                keyboard.insert(InlineKeyboardButton(text="Вперёд", callback_data="next_page"))
+
+        finally:
+            all_goods_keyboards[page] = keyboard
+            page += 1
+
+    return all_goods_keyboards
